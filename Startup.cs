7aadc6239c63388674b756f7cd90a69ed0ac3b74
Ekta Gupta <ekta.gupta@wipfli.com>
2021-05-27 14:42:49 +0530

    Sec-3 API side change

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace DatingApp.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(_config.GetConnectionString("defaultConnection"));
@@ -53,6 +53,9 @@ namespace DatingApp.Api
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            //Allowing Every Heards and Every Methods but only ordgin localhost 4200
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
